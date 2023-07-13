2de692811048da5aaec6ee59750349f2d104f148
Pramod <pramodtarpe12@gmail.com>
2023-07-13 23:14:53 +0530

    get endpoint added

--- src/main/java/com/example/spring_demo/DemoController.java
+++ src/main/java/com/example/spring_demo/DemoController.java
@@ -1,8 +1,13 @@
 package com.example.spring_demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+    @GetMapping("test")
+    public String test(){
+        return "Hello World";
+    }
 }
